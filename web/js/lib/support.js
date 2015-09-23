8011e3f5c184ff89f334a14ce3e884553b4863f0
Jeremy Darling <jeremy.darling@gmail.com>
2015-09-23 16:57:20 -0500

    Add toCSV helper

--- web/js/lib/support.js
+++ web/js/lib/support.js
@@ -405,5 +405,95 @@
       Object.keys(src).filter(notOmit).forEach(copyValue);
       return temp;
     },
+
+    toCSV: function(data, options){
+      /*
+      options.delim = options.delim || ',';
+        // Delimiter between fields defaults to ,
+      options.textDelim = options.textDelim || '"';
+        // Text delimiter to wrap text in, defaults to "
+      options.eoln = options.eoln || '\r\n';
+        // Line end character, defaults \r\n
+      options.headerDelim = options.headerDelim || '_';
+        // Delimiter when joining complex object type headings, defaults _
+      options.escapeTextDelim = options.escapeTextDelim || ('\\'+options.textDelim);
+        // Escape character for embedded text delimiter defaults to \" or \<textDelim>
+      */
+      var headers = [];
+      var records = [];
+      var line;
+      var response = '';
+      var i, l = data.length, rl=0, j, k;
+      var reReplaceTextDelim;
+      var processRecord = function(src, rec, prefix){
+        var key, val, idx;
+        prefix = prefix || '';
+        rec = rec || new Array(headers.length);
+        for(key in src){
+          val = src[key];
+          key = prefix+key;
+          switch(typeof(val)){
+            case('object'):
+              if(val instanceof Array){
+                processRecord(val, rec, key+options.headerDelim);
+                break;
+              }else if(!(val instanceof Date)){
+                processRecord(val, rec, key+options.headerDelim);
+                break;
+              }
+            default:
+              if((idx = headers.indexOf(key))===-1){
+                idx = headers.length;
+                headers.push(key);
+              }
+              rec[idx] = val.toString();
+          }
+        }
+
+        return rec;
+      };
+
+      options = options || {};
+      options.delim = options.delim || ',';
+      options.textDelim = options.textDelim || '"';
+      options.eoln = options.eoln || '\r\n';
+      options.headerDelim = options.headerDelim || '_';
+      options.escapeTextDelim = options.escapeTextDelim || ('\\'+options.textDelim);
+      reReplaceTextDelim = new RegExp(options.textDelim, 'gi');
+
+      if(options.includeHeaders === void(0)){
+        options.includeHeaders = true;
+      }
+
+      for(i=0; i<l; i++){
+        line = processRecord(data[i]);
+        if(rl < line.length){
+          rl = line.length;
+        }
+        records.push(line);
+      }
+      if(options.includeHeaders){
+        response = options.textDelim + headers.join(options.textDelim + options.delim + options.textDelim) + options.textDelim + options.eoln;
+      }
+      for(i=0; i<l; i++){
+        line = records[i];
+        if(line.length<rl){
+          line = line.concat(new Array(rl-line.length));
+        }
+        k = line.length;
+        for(j=0; j<k; j++){
+          if(line[j] !== void(0)){
+            response += options.textDelim + line[j].replace(reReplaceTextDelim, options.escapeTextDelim) + options.textDelim;
+          }
+          if(j<k-1){
+            response += options.delim;
+          }
+        }
+        response += options.eoln;
+      }
+
+      return response;
+    },
+
   };
 })(this);
